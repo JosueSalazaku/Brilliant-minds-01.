5f3d7abef437c84425f3a389b14b2b8f7a2d0924
Josué Salazaku <josuesalazaku@gmail.com>
2023-12-18 10:00:55 +0100

    Server running on port 3000

--- server/server.js
+++ server/server.js
@@ -1,1 +1,25 @@
 const express = require("express");
+const app = express();
+/** TODO:
+
+have a new database brilliant_minds
+Inside that database a table "ideas" with the columns:
+title (VARCHAR(255))
+description (TEXT)
+created_at (Timestamp)
+Create a route for:
+the new idea page (create)
+the landing page (read)
+the delete button (delete)
+Make a database connection in express
+Tip: Only use one JS file for now. */
+
+const PORT = 3000;
+
+app.get("/", function (req, res) {
+  res.send("Hello wold");
+});
+
+app.listen(PORT, () =>
+  console.log(`Server started: http://localhost:${PORT}/`)
+);
