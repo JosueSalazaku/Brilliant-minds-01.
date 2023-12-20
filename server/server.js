e7b62f191ec9f5841a024e9e6e55a6029db2411b
Josué Salazaku <josuesalazaku@gmail.com>
2023-12-20 09:39:45 +0100

    Connect to postman

--- server/server.js
+++ server/server.js
@@ -1,30 +1,39 @@
 import express from "express";
 import mariadb from "mariadb";
-import * as dotenv from "dotenv";
+import { config } from "dotenv";
 
-dotenv.config();
+config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3050;
 const HOST = process.env.HOST || "localhost";
 const app = express();
+
+console.log(process.env.DB_USER);
 
 const pool = mariadb.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   connectionLimit: 5,
 });
 
 app.use(express.json());
 
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
+  res.send("Hello, this is the root page");
+});
+
+// Read all ideas
+app.get("/ideas", async (req, res) => {
   let connection;
   try {
     connection = await pool.getConnection();
-    const data = await connection.query(`SELECT * FROM brilliant_minds.ideas`);
-    res.send(data);
+    const data = await connection.query(`SELECT * FROM ideas`);
+    res.json(data);
   } catch (err) {
-    throw err;
+    console.error("Error in /ideas GET:", err);
+    res.status(500).send("Internal Server Error");
   } finally {
     if (connection) connection.end();
   }
@@ -34,13 +43,64 @@ app.get("/ideas/:id", async (req, res) => {
   let connection;
   try {
     connection = await pool.getConnection();
-    const prepare = await connection.prepare(
-      "SELECT * FROM ideas WHERE id = ?"
-    );
-    const data = prepare.execute([req.params.id]);
-  } catch (error) {}
+    const data = await connection.query("SELECT * FROM ideas WHERE id = ?", [
+      req.params.id,
+    ]);
+
+    if (data.length > 0) {
+      res.json(data[0]);
+    } else {
+      res.status(404).send("Idea not found");
+    }
+  } catch (err) {
+    console.error("Error in /ideas/:id GET:", err);
+    res.status(500).send("Internal Server Error");
+  } finally {
+    if (connection) connection.end();
+  }
 });
 
-app.listen(PORT, () =>
-  console.log(`Server started: http://localhost:${PORT}/`)
-);
+app.post("/ideas", async (req, res) => {
+  const { title, description } = req.body;
+  let connection;
+  try {
+    connection = await pool.getConnection();
+    const data = await connection.query(
+      "INSERT INTO ideas (title, description) VALUES (?, ?)",
+      [title, description]
+    );
+
+    res
+      .status(201)
+      .json({ message: "Idea was added successfully", result: data });
+  } catch (err) {
+    console.error("Error in /ideas POST:", err);
+    res.status(500).send("Server Error");
+  } finally {
+    if (connection) connection.end();
+  }
+});
+
+// Delete an idea by ID
+app.delete("/ideas/:id", async (req, res) => {
+  let connection;
+  try {
+    connection = await pool.getConnection();
+    const data = await connection.query("DELETE FROM ideas WHERE id = ?", [
+      req.params.id,
+    ]);
+
+    if (data.affectedRows > 0) {
+      res.json({ message: "Idea deleted successfully", result: data });
+    } else {
+      res.status(404).send("Idea not found");
+    }
+  } catch (err) {
+    console.error("Error in /ideas/:id DELETE:", err);
+    res.status(500).send("Server Error");
+  } finally {
+    if (connection) connection.end();
+  }
+});
+
+app.listen(PORT, () => console.log(`Server started: http://${HOST}:${PORT}/`));
